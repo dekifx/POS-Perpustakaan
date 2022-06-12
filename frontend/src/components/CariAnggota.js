16159a762a65cb188d8303047c8f451db2906c05
dekifx <uchihaclan188@gmail.com>
2022-06-12 15:20:50 +0800

    [frontend] add Search Feature (Buku, Anggota)

---
+++ frontend/src/components/CariAnggota.js
@@ -0,0 +1,50 @@
+import "../style/input.css";
+import React, { useState } from "react";
+import Axios from "axios";
+
+function CariAnggota() {
+  const [daftar, lihatDaftar] = useState([]);
+  const [teks, setTeks] = useState();
+
+  const cari = (teks_pass) => {
+    Axios.get(`http://localhost:3001/anggota/cari/${teks_pass}`).then(
+      (response) => {
+        console.log = response;
+        lihatDaftar(response.data);
+      }
+    );
+  };
+
+  return (
+    <div>
+      <div className="search">
+        <input
+          type="text"
+          name="pencarian"
+          placeholder="Masukkan Nama"
+          onChange={(event) => {
+            setTeks(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            cari(teks);
+          }}
+        >
+          Cari
+        </button>
+        {daftar.map((val, key) => {
+          return (
+            <div>
+              {val.nama}
+              {val.alamat}
+              {val.no_telp}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default CariAnggota;
---
+++ frontend/src/components/CariBuku.js
@@ -0,0 +1,51 @@
+import "../style/input.css";
+import React, { useState } from "react";
+import Axios from "axios";
+
+function CariBuku() {
+  const [daftar, lihatDaftar] = useState([]);
+  const [teks, setTeks] = useState();
+
+  const cari = (teks_pass) => {
+    Axios.get(`http://localhost:3001/buku/cari/${teks_pass}`).then(
+      (response) => {
+        console.log = response;
+        lihatDaftar(response.data);
+      }
+    );
+  };
+
+  return (
+    <div>
+      <div className="search">
+        <input
+          type="text"
+          name="pencarian"
+          placeholder="Masukkan Judul Buku"
+          onChange={(event) => {
+            setTeks(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            cari(teks);
+          }}
+        >
+          Cari
+        </button>
+        {daftar.map((val, key) => {
+          return (
+            <div>
+              {val.judul}
+              {val.penerbit}
+              {val.pengarang}
+              {val.tanggal}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default CariBuku;
