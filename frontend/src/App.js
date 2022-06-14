d8d6ee8e64758e5e8294675742b44fc151a3e5a9
dekifx <uchihaclan188@gmail.com>
2022-06-14 15:14:20 +0800

    [cleanup] Cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
-import "./App.css";
+import "./style/App.css";
 import React from "react";
 import TambahBuku from "./components/TambahBuku";
 import TambahAnggota from "./components/TambahAnggota";
 import DaftarBuku from "./components/ManajemenBuku";
 import DaftarAnggota from "./components/ManajemenAnggota";
 import DataPeminjaman from "./components/DataPeminjaman";
-import CariBuku from "./components/CariBuku";
-import CariAnggota from "./components/CariAnggota";
 
 function App() {
   return (
     <div className="App">
-      <CariBuku />
-      <CariAnggota />
       <TambahBuku />
       <TambahAnggota />
       <DaftarBuku />
